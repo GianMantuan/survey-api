b47514f954da15180ddc76edfdc3a85dec4ab257
Gian Carlo Mantuan <giancarlo.mdr@gmail.com>
2021-04-01 00:27:11 -0300

    test: add email validation for SignUpController

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -18,4 +18,20 @@ describe('SignUp COntroller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('should return 400 if no email is provided', () => {
+    const password = faker.internet.password()
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: faker.name.findName(),
+        password,
+        passworfConfirmation: password
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })

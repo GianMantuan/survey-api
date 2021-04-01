650dbf4ed699bb9c7c73457d42c4342c43f2e4e9
Gian Carlo Mantuan <giancarlo.mdr@gmail.com>
2021-04-01 00:18:07 -0300

    test: ensurte SignUpController return 400

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,20 @@
+import { SignUpController } from './signup'
+
+import faker from 'faker'
+
+describe('SignUp COntroller', () => {
+  test('should return 400 if no name is provided', () => {
+    const password = faker.internet.password()
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: faker.internet.email(),
+        password,
+        passworfConfirmation: password
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})

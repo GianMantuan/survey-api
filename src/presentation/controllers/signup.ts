21e6e08c3a5a0972c698a09f7f43fd0b32353ee8
Gian Carlo Mantuan <giancarlo.mdr@gmail.com>
2021-04-02 15:55:44 -0300

    feat: add SignUpController to validate email and add InvalidParamsError

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,17 @@
 import { MissingParamError } from '../errors/missing-param'
 import { IHttpRequest, IHttpResponse } from '../protocols/http'
 import { badRequest } from '../helpers/http-helper'
-import { Controller } from '../protocols/Controller'
+import { Controller } from '../protocols/controller'
+import { IEmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param'
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: IEmailValidator
+
+  constructor (emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: IHttpRequest): IHttpResponse {
     const requiredFileds = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFileds) {
@@ -11,6 +19,12 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
+    }
+
     return { statusCode: 200, body: {} }
   }
 }
